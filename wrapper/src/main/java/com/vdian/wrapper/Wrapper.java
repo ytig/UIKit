5550283c9c63499a3f77591baf343c8ef64ed0b0
张亮 <zhangliang01@weidian.com>
2017-11-14 16:20:14 +0800

    wrapper

---
+++ wrapper/src/main/java/com/vdian/wrapper/Wrapper.java
@@ -0,0 +1,12 @@
+package com.vdian.wrapper;
+
+/**
+ * Created by zhangliang on 16/12/14.
+ */
+public interface Wrapper {
+    int NO_POSITION = -1;
+
+    Object getChild();
+
+    int toChildPosition(int position);
+}
